76b2e99312b9682a7ba6fd87762f69df2e553ad9
CougHifi <76088789+CougHifi@users.noreply.github.com>
2023-03-12 20:57:54 -0700

    Update WeeklyPlan.h

--- Fitbit Application/PA 4/PA 4/WeeklyPlan.h
+++ Fitbit Application/PA 4/PA 4/WeeklyPlan.h
@@ -1,43 +1,5 @@
 #include "Libraries.h"
-/*Weekly plan will have the same member functions as the dietplan so use either inheritance
-or just hard code it in. Might not need the manager stuff.*/
-//class WeeklyPlan
-//{
-//private:
-//	double mNewset;
-//	int mNewworkout;
-//	string mName;
-//	string mDateCreated;
-//	string mWorkout;
-//public:
-//	/*This is the weeklyplans information*/
-//	WeeklyPlan(double initaldietplan = 0.0, int Newworkout = 0 /* might need to make this into a char */,
-//		/*you will also be asking the user for the days.*/
-//		string name = "",
-//		string day = "",
-//		string datecreated = "",
-//		string theworkoutduration = "");
-//	WeeklyPlan(WeeklyPlan &copyDietplan);/*This creates a Shallow copy*/
-//	~WeeklyPlan();
-//
-//	WeeklyPlan &operator= (const WeeklyPlan &rhs);
-//
-//	double getWeeklyplan() const;
-//	int getNewworkout() const;
-//	string getName() const;
-//	string getDateCrated() const;
-//	string getWorkout() const;
-//
-//	void setWeeklyplan(double newWeeklyplan);
-//	void setNewworkout(int Newworkout);
-//	void setName(string newName);
-//	void setDatecreated(string newDateCreated);
-//	void setWorkout(string newWorkout);
-//
-//	double days(double newDays);
-//	double time(double newTime);
-//	void printWeeklyplan();
-//};
+
 
 class WeeklyPlan
 {
@@ -69,5 +31,5 @@ public:
 	void setWorkout(string newWorkout1);
 
 	void printWeeklyplan();
-	//double NameOfDay
+	
 };
